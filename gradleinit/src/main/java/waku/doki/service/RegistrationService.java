3611bb4d8fef2b8e005686dd7a4be454e5a4f37f
trainee01 <trainee01@BGPN0280.trainee.local>
2019-07-22 10:57:12 +0900

    文字数を追加するメソッドを追加

--- gradleinit/src/main/java/waku/doki/service/RegistrationService.java
+++ gradleinit/src/main/java/waku/doki/service/RegistrationService.java
@@ -1,5 +1,9 @@
 package waku.doki.service;
 
 public class RegistrationService {
-
+// 強制的にコンフリクトを発せさせるためにtestブランチとmasterブランチに異なる変更を加える
+	public int numCount(int i) {
+		String str = "研修前半お疲れ様です";
+		return i + str.length();
+	}
 }
