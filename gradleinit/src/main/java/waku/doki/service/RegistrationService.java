20e1741fe48d499519cdafb994f5295d48f2e908
trainee01 <trainee01@BGPN0280.trainee.local>
2019-07-22 11:01:30 +0900

    標準出力するメソッドを追加

--- gradleinit/src/main/java/waku/doki/service/RegistrationService.java
+++ gradleinit/src/main/java/waku/doki/service/RegistrationService.java
@@ -1,5 +1,9 @@
 package waku.doki.service;
 
 public class RegistrationService {
-
+// 強制的にコンフリクトを発せさせるためにtestブランチとmasterブランチに異なる変更を加える
+	public void MessageOnConsole(int i) {
+		String str = "研修後半頑張りましょう";
+		System.out.println(str);
+	}
 }
